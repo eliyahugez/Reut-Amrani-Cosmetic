d7d681bf0a499e8338facc77860e5a685bbda01a
eliyahugez <eliyahugez@gmail.com>
2025-06-18 23:24:24 +0300

    ss

--- src/components/PriceListPage.tsx
+++ src/components/PriceListPage.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import price1 from "/assets/priceNew1.png.jpg";
+import price2 from "/assets/priceNew2.jpg";
 
 const PriceListPage: React.FC = () => {
   return (
@@ -12,14 +14,14 @@ const PriceListPage: React.FC = () => {
       <div className="w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-10 items-center justify-center px-2 md:px-0">
         <div className="transition-transform duration-500 hover:scale-105 group relative w-full flex justify-center">
           <img
-            src="/assets/priceNew1.png.jpg"
+            src={price1}
             alt="מחירון 1"
             className="rounded-lg shadow-lg w-4/5 md:w-full object-contain bg-gray group-hover:brightness-110 group-hover:contrast-125 group-hover:rotate-1 animate-fade-out group-hover:shadow-xl shadow-black"
           />
         </div>
         <div className="transition-transform duration-500 hover:scale-105 group relative w-full flex justify-center">
           <img
-            src="/assets/priceNew2.jpg"
+            src={price2}
             alt="מחירון 2"
             className="rounded-lg shadow-lg w-4/5 md:w-full object-contain bg-gray group-hover:brightness-110 group-hover:contrast-125 group-hover:-rotate-1 animate-fade-in group-hover:shadow-xl shadow-black"
           />
